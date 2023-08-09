6f620bf90a5b29bb627b5940d15ad2ae5bb4a192
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-08-09 00:10:24 -0400

    Revert to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
