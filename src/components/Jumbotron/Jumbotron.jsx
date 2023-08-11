fc1423b0fc57ff1430993141b6eafea8ce87f991
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-08-11 15:19:04 -0400

    Modify jumbotron css and placeholder text

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,18 +1,18 @@
 import './jumbotron.css';
-import jumbotron_bg from '../../assets/img/jumbotron-bg.jpg';
+import jumbotron_bg from '../../assets/img/jumbotron-img.jpg';
 
 export const Jumbotron = () => {
   return (
     <>
       <div className="container mb-4">
         <div className="row">
-          <div className="col-md-7">
+          <div className="col-lg-7">
               <img className="img-fluid rounded-custom" src={jumbotron_bg} alt="Cupcakes verde menta sobre una mesa rosada" />
           </div>
-          <div className="col-md-5 p-5">
-            <h2 className='resp-title mb-4'>Nubes de Azúcar</h2>
-            <p className='resp-p mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            <button className="btn jumbotron-btn resp-btn">Haz tu pedido ahora</button>
+          <div className="col-lg-5 p-4">
+            <h2 className='resp-title mb-4'>Un rincón acogedor donde los sabores se entrelazan con sonrisas</h2>
+            <p className='resp-p mb-4'>Descubre nuestro rincón lleno de dulzura y cariño, donde cada mordisco es un recuerdo y cada sorbo te invita a saborear la vida. Ven a comenzar tus días con una sonrisa y a relajarte en nuestras acogedoras mesas al final de cada jornada.</p>
+            <button className="btn jumbotron-btn resp-btn">Reserva tu momento especial</button>
           </div>
         </div>
       </div>
