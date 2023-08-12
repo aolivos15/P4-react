b2bbb0156cc68ece82e228888ab6d8ac004970a3
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-08-11 22:04:22 -0400

    Modify header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import './header.css'
 export const Header = ({title}) => {
     return (
         <>
-            <div className="container mb-4">
-                <div className="header rounded-custom dash-border">
+            <div className="container mt-4">
+                <div className="header rounded-custom">
                     <h1>{title}</h1>
                 </div>
             </div>
