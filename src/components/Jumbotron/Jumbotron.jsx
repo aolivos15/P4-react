1d602d088050898e3366a5efd052b8b62ec4bb2c
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-08-12 18:56:16 -0400

    Add link to booking page to jumbotron

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,5 +1,6 @@
 import './jumbotron.css';
 import jumbotron_bg from '../../assets/img/jumbotron-img.jpg';
+import { Link } from 'react-router-dom';
 
 export const Jumbotron = () => {
   return (
@@ -12,7 +13,7 @@ export const Jumbotron = () => {
           <div className="col-lg-5 p-4">
             <h2 className='resp-title mb-4'>Un rincón acogedor donde los sabores se entrelazan con sonrisas</h2>
             <p className='resp-p mb-4'>Descubre nuestro rincón lleno de dulzura y cariño, donde cada mordisco es un recuerdo y cada sorbo te invita a saborear la vida. Ven a comenzar tus días con una sonrisa y a relajarte en nuestras acogedoras mesas al final de cada jornada.</p>
-            <button className="btn jumbotron-btn resp-btn">Reserva tu momento especial</button>
+            <Link to="/reservas" className='jumbotron-link'><button className="btn jumbotron-btn resp-btn">Reserva tu momento especial</button></Link>
           </div>
         </div>
       </div>
